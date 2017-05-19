ba5db22a07cb52adf66f7e87b9dc8aef7d0ef50b
Marcin Wyszynski <marcin.pixie@gmail.com>
2017-05-19 13:35:58 +0200

    Fix deploy steps (#1)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-gem build closing_comments.gemspec
+gem build ensure_version_bump.gemspec
 echo "---" > ~/.gem/credentials
 echo ":rubygems_api_key: ${RUBYGEMS_API_KEY}" >> ~/.gem/credentials
 chmod 600 ~/.gem/credentials
